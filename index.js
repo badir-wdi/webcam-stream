c6a47c23fde872e7c6cf5a02d37a611d1ce96437
Salman778 <alk-333@hotmail.com>
2019-09-26 08:51:07 +0300

    delet everything for chart

--- index.js
+++ index.js
@@ -6,66 +6,7 @@
 
   function success( result ) {
     const {anger, disgust, fear, happiness, sadness, surprise} = result.people[0].emotions
-    // Themes begin
-    am4core.useTheme(am4themes_animated);
-    // Themes end
-    
-    // Create chart instance
-    var chart = am4core.create("chartdiv", am4charts.XYChart);
-    
-    // Add data
-    chart.data = [{
-      "emotion": "anger",
-      "expression": anger
-    }, {
-      "emotion": "disgust",
-      "expression": disgust
-    }, {
-      "emotion": "fear",
-      "expression": fear
-    }, {
-      "emotion": "happiness",
-      "expression": happiness
-    }, {
-      "emotion": "sadness",
-      "expression": sadness
-    }, {
-      "emotion": "surprise",
-      "expression": surprise
-    },
-    {
-      "emotion": "",
-      "expression": 90
-    }
-  ];
-    
-    // Create axes
-    
-    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "emotion";
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.minGridDistance = 30;
-    
-    categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
-      if (target.dataItem && target.dataItem.index & 2 == 2) {
-        return dy + 25;
-      }
-      return dy;
-    });
-    
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    
-    // Create series
-    var series = chart.series.push(new am4charts.ColumnSeries());
-    series.dataFields.valueY = "expression";
-    series.dataFields.categoryX = "emotion";
-    series.name = "Expression";
-    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
-    series.columns.template.fillOpacity = .8;
-    
-    var columnTemplate = series.columns.template;
-    columnTemplate.strokeWidth = 2;
-    columnTemplate.strokeOpacity = 1;
+    // Here put your chart
   }
 
   function failure( errorCode, errorString ) {
